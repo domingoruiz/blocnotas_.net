b50c407790de8aef470b650aa996ff9e8c5eb67b
Domingo Ruiz Arroyo <ordenadordomi@gmail.com>
2021-04-19 20:17:59 +0200

    Primera versión. Falta Creado Automático Carpeta.

---
+++ Funciones.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlocNotasDomingo
+{
+    class Funciones
+    {
+        public static void escribir_cabecera()
+        {
+            Console.Clear();
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("------- Bloc de notas \"Domingo\" -------");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("");
+        }
+
+        public static void volver_inicio()
+        {
+            Console.ReadLine();
+            main.Main();
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace BlocNotasDomingo
+{
+    class main
+    {
+        public static void Main()
+        {
+            String modo = Operaciones.pantallainicio();
+
+            if (modo == "1")
+            {
+                Operaciones.vernotasexistentes();
+            }
+            else if (modo == "2")
+            {
+                Operaciones.crearnota();
+            }
+            else if (modo == "3")
+            {
+                Operaciones.eliminarnota();
+            }
+            else if (modo == "4")
+            {
+                Operaciones.salir(); 
+            }
+            else
+            {
+                Operaciones.operacionnoexistente();
+            }
+        }
+    }
+}
